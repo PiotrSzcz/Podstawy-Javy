a1d577178a640a1eb0b122adf4696340c3e2e531
PiotrSzcz <pioszc77@wp.pl>
2020-01-07 20:59:10 +0100

    typy zmiennych

--- src/podstawy/Main.java
+++ src/podstawy/Main.java
@@ -3,6 +3,26 @@ package podstawy;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(2);
+		
+														// \u0001 unicode w javie
+		boolean bWartoscLogiczna = true; 
+		int[] TabelaLiczb = {1,1,2};  					// klamry przy typie 
+		String sLancuch = "Dowolny ci¹g znaków";		// typ String nie jest typem podstawowym
+		int z = 1;
+		double dWartosc = 3.14;
+		
+		int y = (int)dWartosc;							//zmiana typu
+		double dZmiana = z;
+		
+		
+		System.out.println("Hello World");
+		System.out.println(dWartosc + "; " + y + "; " + dZmiana);
+
+		
+		if(bWartoscLogiczna) {
+			for(int x :TabelaLiczb ) {
+				System.out.print(TabelaLiczb[x] + " ");
+			}
+		}
 	}
 }
